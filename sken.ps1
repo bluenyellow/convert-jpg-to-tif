46e0e62ccc82ec0834d8311aaf16490159f44815
bluenyellow <alex@adamica.net>
2020-08-31 13:45:09 +0200

    Update sken.ps1

--- sken.ps1
+++ sken.ps1
@@ -7,39 +7,32 @@ Objective was converting appeared scans(jpegs) from printer to tif.
 
 Add-Type -AssemblyName System.Windows.Forms
 
-net use H: \\path\to\folder
-cd H:\
-$cesta = pwd | Select -expandproperty path
+$path='C:\path\to\folder\'
 
-$jpgs = get-childitem H:\ -recurse | where {$_.Extension -match "jpg"} | Where ({$_.Name -match '^scan\d+' -or $_.Name -match '^sken\d+'})
 
-Start-Sleep -s 5
+#using regex to parse jpgs with name in form scan or sken with numbers after it
+$jpgs = get-childitem $path -recurse | where {$_.Extension -match "jpg"} | Where ({$_.Name -match '^scan\d+' -or $_.Name -match '^sken\d+'})
 
-$jpgs2 = get-childitem H:\ -recurse | where {$_.Extension -match "jpg"} | Where ({$_.Name -match '^scan\d+' -or $_.Name -match '^sken\d+'})
+Start-Sleep -s 5 #wait 5 seconds if any jpgs appear from printer...
 
-if($jpgs2.count -eq $jpgs.count){
+#in jpgs2 we parse jpgs files again
+$jpgs2 = get-childitem $path -recurse | where {$_.Extension -match "jpg"} | Where ({$_.Name -match '^scan\d+' -or $_.Name -match '^sken\d+'})
+
+
+if($jpgs2.count -eq $jpgs.count){    #here we compare count of jpgs and jpgs2 and if its equal we convert jpeg files
 foreach($jpg in $jpgs){
 
-	$filepath = "$($cesta)\$($jpg.Name)"
+	$filepath = "$($path)\$($jpg.Name)"
 
 
 	$picture = [System.Drawing.Bitmap]($filepath)
-	$picture.Save("H:\$($jpg.BaseName).tif",'TIFF')
+	$picture.Save("$path\$($jpg.BaseName).tif",'TIFF')
 	$picture.Dispose()
 
 }
 
-
-cd C:\
-
-
 #deleting the old jpegs
 $jpgs | Remove-item -force
 
-#net use H: /delete 
-}
-
-else{
-cd C:
-#net use H: /delete
+ 
 }
